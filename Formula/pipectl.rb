10bf60f90d10324562ce0b1b0a76603a9e57db6d
Shohei Ueda <30958501+peaceiris@users.noreply.github.com>
2024-04-19 00:26:28 +0900

    Add sha256

--- Formula/pipectl.rb
+++ Formula/pipectl.rb
@@ -10,6 +10,7 @@ class Pipectl < Formula
     on_macos do
       if Hardware::CPU.intel?
         url "https://github.com/pipe-cd/pipecd/releases/download/v0.47.0/pipectl_v0.47.0_darwin_amd64"
+        sha256 "922a7e4cd4e3c79e4ce9c16bfbe7849493691cf0b8a4be6874c3c3bb341efabc"
 
         def install
           bin.install "pipectl_v0.47.0_darwin_amd64" => "pipectl"
@@ -17,6 +18,7 @@ class Pipectl < Formula
       end
       if Hardware::CPU.arm?
         url "https://github.com/pipe-cd/pipecd/releases/download/v0.47.0/pipectl_v0.47.0_darwin_arm64"
+        sha256 "b3c3efcd4b4b1a611e320ebd7613705f919ae793167f8f40a22826431b9d6c5f"
 
         def install
           bin.install "pipectl_v0.47.0_darwin_arm64" => "pipectl"
@@ -27,6 +29,7 @@ class Pipectl < Formula
     on_linux do
       if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
         url "https://github.com/pipe-cd/pipecd/releases/download/v0.47.0/pipectl_v0.47.0_linux_arm64"
+        sha256 "cca096cfff8ea95a908eb69f1bf6e7abbabb7cb0d3de4e5763f0b438c8a34a95"
 
         def install
           bin.install "pipectl_v0.47.0_linux_arm64" => "pipectl"
@@ -34,6 +37,7 @@ class Pipectl < Formula
       end
       if Hardware::CPU.intel?
         url "https://github.com/pipe-cd/pipecd/releases/download/v0.47.0/pipectl_v0.47.0_linux_amd64"
+        sha256 "58bd709010df521f378d8290f3317424472bb6494d7748b1bb4740fea5db4d7f"
 
         def install
           bin.install "pipectl_v0.47.0_linux_amd64" => "pipectl"
